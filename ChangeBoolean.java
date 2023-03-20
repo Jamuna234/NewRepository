ae6bef0c9d9276dc53f862e91312fff83dd537e1
Jamuna234 <jkjeminakumar23@gmail.com>
2023-03-20 17:19:53 +0530

    success

--- ChangeBoolean.java
+++ ChangeBoolean.java
@@ -2,7 +2,7 @@ package com.day4;
 
 public class ChangeBoolean {
 public static void main (String [] args) {
-	boolean lp= Boolean.parseBoolean(args[0]);
+	boolean lp= Boolean.parseBoolean(args[1]);
 	System.out.println(lp);
 }
 }
