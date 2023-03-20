8c363fea550c308d0f25a378fad151a3119c514e
Jamuna234 <jkjeminakumar23@gmail.com>
2023-03-20 17:06:44 +0530

    update program

--- Adding.java
+++ Adding.java
@@ -3,7 +3,7 @@ package com.day4;
 public class Adding {
 	public static void main(String[] args) {
 		int no=Integer.parseInt(args[0]);
-		System.out.println(no+50);
+		System.out.println(no+100);
 		
 	}
 
